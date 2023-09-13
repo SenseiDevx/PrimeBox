e135a425c2265703c6cd691e4bb054c49955fdd1
sensei404 <kurmanbekovomurbek80@gmail.com>
2023-09-13 22:28:19 +0600

    push v.1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react'
+
+const Footer = () => {
+
+
+    return (
+        <>
+            <footer className='bg-theme pt-7 pb-5'>
+                <div className='nike-container text-slate-200'>
+                    <h2 className='text-center font-bold text-7xl sm:text-2xl'>Made by Omurbek</h2>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
