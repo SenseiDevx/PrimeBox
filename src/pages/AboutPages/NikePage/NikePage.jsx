e916e935e1798e6bed8ba8fc37a10db58442f7e7
sensei404 <kurmanbekovomurbek80@gmail.com>
2023-09-17 22:03:02 +0600

    почти закончил

--- src/pages/AboutPages/NikePage/NikePage.jsx
+++ src/pages/AboutPages/NikePage/NikePage.jsx
@@ -27,9 +27,21 @@ const NikePage = ({ifExists}) => {
     return (
         <>
             <div className="nike-container mt-24">
-                <div className="flex justify-between lg:flex-col">
-                    <div className="flex flex-wrap-reverse items-end">
-                        <div className="mr-11 lg:flex">
+                <div className="flex flex-wrap-reverse items-en">
+                    <div>
+                        <div
+                            className={`relative bg-gradient-to-b ${product.color} ${product.shadow} grid items-center ${
+                                ifExists ? "justify-items-start" : "justify-items-center"
+                            } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105 my-container`}
+                            style={{height: "300px", marginBottom: "50px"}}
+                        >
+                            <img
+                                src={selectedImage}
+                                alt={product.title}
+                                className="w-400 h-400 sm:w-300 sm:h-300 relative rounded-xl py-4 px-5 transition-all duration-700 ease-in-out hover:scale-105 lg:flex justify-center items-center"
+                            />
+                        </div>
+                        <div className="mr-11 flex">
                             {product.images.map((image, i) => (
                                 <div
                                     key={i}
@@ -46,49 +58,55 @@ const NikePage = ({ifExists}) => {
                                 </div>
                             ))}
                         </div>
-                        <div>
-                            <div
-                                className={`relative bg-gradient-to-b ${product.color} ${product.shadow} grid items-center ${
-                                    ifExists ? "justify-items-start" : "justify-items-center"
-                                } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105 my-container`}
-                                style={{ height: "300px", marginBottom: "50px" }}
-                            >
-                                <img
-                                    src={selectedImage}
-                                    alt={product.title}
-                                    className="w-400 h-400 sm:w-300 sm:h-300 relative rounded-xl py-4 px-5 transition-all duration-700 ease-in-out hover:scale-105 lg:flex justify-center items-center"
-                                />
+                        <div className="flex-wrap">
+                            <h1 className="text-2xl text-white pb-2">Выбирайте ваш размер</h1>
+                            <div className="flex gap-3 flex-wrap mb-10 w-100">
+                                {product.sizes.map((size, i) => (
+                                    <div
+                                        key={i}
+                                        className={`border-2 w-45 h-45 p-1.5 rounded text-white cursor-pointer ${
+                                            selectedSize === size ? 'bg-blue-500 text-black' : ''
+                                        }`}
+                                        onClick={() => setSelectedSize(size)}
+                                    >
+                                        {size}
+                                    </div>
+                                ))}
                             </div>
-                            <div className="flex-wrap">
-                                <h1 className="text-2xl text-white pb-2">Выбирайте ваш размер</h1>
-                                <div className="flex gap-3 flex-wrap mb-10 w-100">
-                                    {product.sizes.map((size, i) => (
-                                        <div
-                                            key={i}
-                                            className={`border-2 w-45 h-45 p-1.5 rounded text-white cursor-pointer ${
-                                                selectedSize === size ? 'bg-blue-500 text-black' : ''
-                                            }`}
-                                            onClick={() => setSelectedSize(size)}
-                                        >
-                                            {size}
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-
                         </div>
-                        <div>
-                            <h1 className="text-white">{product.title}</h1>
-                            <p className="text-white">{product.text}</p>
+                    </div>
+                    <div>
+                        <h1 className="text-white text-3xl flex items-center">
+                            {product.title}
+                        </h1>
+                        <h1 className="text-white text-2xl flex items-center">
+                            {product.text}
+                        </h1>
+                        <div
+                            className={`relative bg-gradient-to-b ${product.color} ${product.shadow} grid items-center ${
+                                ifExists ? "justify-items-start" : "justify-items-center"
+                            } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full`}
+                            style={{
+                                width: "180px",
+                                display: "flex",
+                                justifyContent: "center",
+                                marginTop: "15px",
+                                marginBottom: "40px"
+                            }}>
+                            <p className="text-white text-3xl">{product.price} <span className="text-opacity-60 text-gray-100 text-3xl">Сом</span></p>
                         </div>
                     </div>
                 </div>
+                <div className="border-2 border-blue-400 p-5 w-96 rounded-2xl sm:w-64">
+                    <h3 className="text-white mb-5 text-2xl sm:text-1xl">Название: {product.title}</h3>
+                    <p className="text-white mb-5 text-2xl sm:text-1xl">Размер: {selectedSize ? selectedSize : 'Выберите размер'}</p>
+                    <button className={`relative bg-gradient-to-b ${product.color} ${product.shadow} grid items-center ${
+                        ifExists ? "justify-items-start" : "justify-items-center"
+                    } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full`}
+                            style={{width: "150px", color: "white"}}
+                    >Купить товар</button>
+                </div>
             </div>
-            <div className="nike-container">
-                <h3 className="text-white">Название: {product.title}</h3>
-                <p className="text-white">Размер: {selectedSize ? selectedSize : 'Выберите размер'}</p>
-            </div>
-
             <div className="mt-24">
                 <NikeSales nikeProduct={nikeProduct}/>
             </div>
